c72362377636b2d7ba9fc4493aa4bc432669a059
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-07-16 16:19:52 +0530

    Added cookie parse functionality

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const path = require("path");
 const mongoose = require("mongoose");
 const User = require("./models/User");
 const bcrypt  = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
+const SECRET="garv";
 
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/auth')
@@ -20,7 +24,9 @@ main()
 
 app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"))
+app.set("views",path.join(__dirname,"views"));
+app.use(cookieParser());
+
 
 
 
@@ -61,6 +67,56 @@ app.get("/login",(req,res)=>{
     res.render("login.ejs");
 })
 
+app.post("/login",async(req,res)=>{
+    let {username,password}=req.body;
+
+    let user = await User.findOne({username});
+
+    if(!user){
+        return res.send("User does not exists please register yourself first");
+    }
+
+    const match = await bcrypt.compare(password,user.password);
+
+    if(!match){
+        return res.send("Wrong password entered");
+    }
+
+    let token = jwt.sign(
+        {
+            id:user._id,
+            username:user.username
+        },
+        SECRET,
+        {
+            expiresIn:"1h"
+        }
+    )
+
+    res.cookie("token",token);
+
+    res.redirect("/profile");
+})
+
+app.get("/profile",(req,res)=>{
+   
+    let token = req.cookies.token;
+
+    if(!token){
+        return res.redirect("/login");
+    }
+
+    try{
+        const data = jwt.verify(token,SECRET);
+
+        res.render("profile",{username:data.username})
+    }
+    catch{
+
+        res.redirect("/login");
+
+    }
+})
 
 app.listen(port,()=>{
     console.log("Listening to port 8080");
