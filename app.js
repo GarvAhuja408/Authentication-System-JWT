14d20ee2deeb9998922d6a92d4c50188df1b81b9
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-07-16 16:37:58 +0530

    Added styling

--- app.js
+++ app.js
@@ -26,14 +26,14 @@ app.use(express.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 
 
 
 
 
-
-app.get("/",(req,res)=>{
+app.get("/home",(req,res)=>{
     res.render("home.ejs");
 })
 
@@ -116,6 +116,12 @@ app.get("/profile",(req,res)=>{
         res.redirect("/login");
 
     }
+})
+
+
+app.get("/logout",(req,res)=>{
+    res.clearCookie("token");
+    res.redirect("/home");
 })
 
 app.listen(port,()=>{
